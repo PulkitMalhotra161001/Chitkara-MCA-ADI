3730419d8b78972029bbf2f6a7b71035a8064d76
Pulkit Malhotra <pulkit161001@gmail.com>
2024-03-27 12:25:02 +0530

    added just greater element file

--- Lecture-9/JustGreaterElement.java
+++ Lecture-9/JustGreaterElement.java
@@ -5,3 +5,29 @@
  Hint: TreeSet
  */
 
+ import java.util.*;
+
+ class HelloWorld {
+     public static void main(String[] args) {
+         int input[] = {3,4,1,7,3,6,2,6,2,6,3,7,1,11,414,1,51,5};
+         int output[] = find(input);
+         for(int i:output){
+             System.out.print(i+" ");
+         }
+     }
+     static int[] find(int input[]){
+         TreeSet<Integer> set = new TreeSet<>();
+         int n=input.length;
+         int output[] = new int[n];
+         for(int i=0;i<n;i++){
+             if(set.higher(input[i])==null){
+                 output[i] = -1;
+             }else{
+                 output[i] = set.higher(input[i]);
+             }
+             
+             set.add(input[i]);
+         }
+         return output;
+     }
+ }

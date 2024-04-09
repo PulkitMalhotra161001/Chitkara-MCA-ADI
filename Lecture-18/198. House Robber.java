e93bfc2f7eccc8c69e3d772e218285d353a23ac8
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2024-04-09 17:27:25 +0530

    Create 198. House Robber.java

---
+++ Lecture-18/198. House Robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length, dp[] = new int[n];
+        dp[0]=nums[0];
+        
+        if(n>1) dp[1]=Math.max(dp[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+}
